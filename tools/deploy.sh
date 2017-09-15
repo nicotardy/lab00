b2e0ac5f59ffcf94437fb498d024cbf7ab425502
Shaiou <shailendra.narayen@d2-si.eu>
2017-09-15 17:00:00 +0200

    Update deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -5,5 +5,6 @@ packer build ____________MYPACKERFILE_________ | tee /tmp/packer.out
 AMI=$(awk -F':' '/(eu|us|ap|sa)-(west|central|east|northeast|southeast)-(1|2): ami-/ {print $2}' /tmp/packer.out |  tr -d '[[:space:]]')
 
 cd $TRAVIS_BUILD_DIR/webapp
+terraform init -var ami_id=$AMI
 terraform plan -var ami_id=$AMI
 terraform apply -var ami_id=$AMI
