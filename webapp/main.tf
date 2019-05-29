81326e5eaef44f28b80facd2f8762327bdf53a87
nta <nico.taroy@gmail.com>
2019-05-29 14:17:57 +0200

    ouverture ssh

--- webapp/main.tf
+++ webapp/main.tf
@@ -18,6 +18,13 @@ resource "aws_security_group" "allow_all" {
   vpc_id      = "${data.terraform_remote_state.rs-vpc.vpc_id}"
 
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
